616d14917c8b06b2cb61c6eef7351e98a77fa1a6
J. Lucas McKay PhD MSCR <j.lucas.mckay@gmail.com>
2021-10-29 14:39:48 -0400

    added redcap integration

--- utilities.R
+++ utilities.R
@@ -1,18 +1,28 @@
-load_report = function(token){
-url <- "https://redcap.emory.edu/api/"
-formData <- list("token"=token,
-                 content='report',
-                 format='json',
-                 report_id='31632',
-                 csvDelimiter='',
-                 rawOrLabel='raw',
-                 rawOrLabelHeaders='raw',
-                 exportCheckboxLabel='false',
-                 returnFormat='json'
+library(tidyverse)
+
+ReadRedcapReport = function(
+  token = Sys.getenv("redcap_token"),
+  url = "https://redcap.emory.edu/api/",
+  report_id = '29228',
+  format='csv',
+  csvDelimiter='',
+  rawOrLabel='raw',
+  rawOrLabelHeaders='raw',
+  exportCheckboxLabel='false',
+  returnFormat='json',
+  ...
 )
-response <- httr::POST(url, body = formData, encode = "form")
-result <- httr::content(response)
-result
-}  
-
-
+{
+  form.data <- list(token=token,
+                    content='report',
+                    format=format,
+                    report_id=as.character(report_id),
+                    csvDelimiter=csvDelimiter,
+                    rawOrLabel=rawOrLabel,
+                    rawOrLabelHeaders=rawOrLabelHeaders,
+                    exportCheckboxLabel=exportCheckboxLabel,
+                    returnFormat=returnFormat
+  )
+  response <- httr::POST(url, body = form.data, encode = "form")
+  readr::read_csv(httr::content(response,as="text"),...)
+}
