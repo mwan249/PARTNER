f6ae7eb245f51b9222ea52f79b6c350b4e53620e
J. Lucas McKay PhD MSCR <j.lucas.mckay@gmail.com>
2021-10-29 14:23:00 -0400

    did some manual changes

---
+++ utilities.R
@@ -0,0 +1,15 @@
+url <- "https://redcap.emory.edu/api/"
+formData <- list("token"=token,
+                 content='report',
+                 format='json',
+                 report_id='31632',
+                 csvDelimiter='',
+                 rawOrLabel='raw',
+                 rawOrLabelHeaders='raw',
+                 exportCheckboxLabel='false',
+                 returnFormat='json'
+)
+response <- httr::POST(url, body = formData, encode = "form")
+result <- httr::content(response)
+print(result)
+
